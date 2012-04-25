66112f9214f6d00c2d53b46cae0bbc4438edf3bf
Olja <oliveravujatovic@yahoo.com>
2012-04-25 18:52:00 +0200

    Gives number and length of fragments.

--- pCut.py
+++ pCut.py
@@ -12,8 +12,21 @@ class Plasmid:
         self.sequence = sequence
         
     def cut(self,RE_site):
+        self.RE_site = RE_site
         fragments = self.sequence.split(RE_site)
         return fragments
+    
+    def number_of_fr(self):
+        fr_number = len(self.sequence.split(self.RE_site))
+        return fr_number
+    
+    def fr_length(self):
+        fragments = self.sequence.split(self.RE_site)
+        for string in fragments:
+            print len(string)
+        return 
+    
+
     
 
     
