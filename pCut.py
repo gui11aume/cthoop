df13627db181851009fb3f92f92b0720c552acf8
Olja <oliveravujatovic@yahoo.com>
2012-04-26 13:02:00 +0200

    Avoiding repeating myself

--- pCut.py
+++ pCut.py
@@ -11,18 +11,16 @@ class Plasmid:
     def __init__(self, sequence):
         self.sequence = sequence
         
-    def cut(self,RE_site):
-        self.RE_site = RE_site
+    def cut(self, RE_site):
         fragments = self.sequence.split(RE_site)
         return fragments
     
-    def number_of_fr(self):
-        fr_number = len(self.sequence.split(self.RE_site))
-        return fr_number
-    
-    def fr_length(self):
-        fragments = self.sequence.split(self.RE_site)
-        for string in fragments:
+    def number_of_fr(self, RE_site):
+        return len(self.cut(RE_site))
+        
+    def fr_length(self, RE_site):
+        x = self.cut(RE_site)
+        for string in x:
             print len(string)
         return 
     
