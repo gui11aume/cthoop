25c831add360121c2e655251b3b574bd02e410df
Heng-Chang Chen <chen.hc_evan@yahoo.de>
2012-04-27 12:29:13 +0200

    initial commit comment

--- inputDNA.py
+++ inputDNA.py
@@ -1,5 +1,42 @@
-# @Hengh-Chang: this is your file. Change the name the way you
-# like. I suggest you create a Class that you could call 'Reader'.
-# This could have a method 'read(self)' that returns a standardized
-# DNA string (ie checks that all letters are G, A, T, C or N, and
-# captilize them etc.
+import re
+
+class Reader:
+  """A class checking a input sequence"""
+
+   #Class variables
+   input_seq:""
+   correct_nucleotide: "ATCGN"
+   
+   #Class methods
+   def __init__(self, input_seq):
+      """remove the first line in data without sequence"""
+      file = open("self.input_seq"))
+      for line in file:
+         if line[0] == ">":
+            pass
+         else:
+            print line
+      return
+  
+   def read_least(self, inout_seq):
+      """remove the least symbol in a sequence"""
+      file = open("self.input_seq")
+      read_seq = file.read()
+      read_seq = read_seq("\n", "")
+      return read_seq
+
+      def seq_check(self, input_seq):
+         """check whether a sequence containing reasonable letters"""
+         self.input_seq = input_seq
+         file = open("self.input_seq")
+         read_seq = file.read()
+         for letter in read_seq:
+            if not letter in self.correct_nucleotide:
+               print "The given sequence contains wrong nucleotides."
+         return
+
+
+if __name__ == '__main__':
+   print(DNA_seq(sys.argv[1]))
+
+
