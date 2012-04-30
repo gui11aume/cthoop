fd29b531cf6818f4f2dc22dc3b162720db2e4f55
Olja <oliveravujatovic@yahoo.com>
2012-04-30 18:20:34 +0200

    It recognizes restr enz sites formed after fusion of the first and the last fragment(more complicated version)

--- pCut.py
+++ pCut.py
@@ -16,12 +16,43 @@ class Plasmid:
     def cut(self, RE_site):
         fragments = self.sequence.split(RE_site)
         n = len(fragments)
+        fragments[0] = fragments[n-1] + fragments[0]
+        del(fragments[n-1])
+        x = [s.split(RE_site) for s in fragments[0]]
         for x in range (n-1):
             fragments[x] += RE_site
-        fragments[n-1] += fragments[0]
-        fragments[0] = fragments[n-1]
-        del(fragments[n-1])
-        
+        new_fragments = fragments[0].split(RE_site)
+        m = len(new_fragments)
+        if m > 1:
+            for i in range (m-1):
+                new_fragments[i] += RE_site
+        else:
+            pass
+        fragments = new_fragments + fragments
+        return fragments
+    
+    def cut_shorter(self, RE_site):
+        double_sequence = 2 * self.sequence
+        x = double_sequence.find(RE_site)
+        n = len(self.sequence)
+        if x>0:
+            fragments = double_sequence[x::]
+            fragments = fragments[:-(n-x)]
+            fragments = fragments.split(RE_site)
+            del(fragments[0])
+            k = len(fragments)
+            for y in range (k):
+                fragments[y] += RE_site
+        else:
+            return "There is no restriction ezyme cut in your sequence"
+        #new_fragments = fragments[0].split(RE_site)
+        #m = len(new_fragments)
+        #if m > 1:
+         #   for i in range (m-1):
+          #      new_fragments[i] += RE_site
+        #else:
+         #   pass
+        #fragments = new_fragments + fragments
         return fragments
     
     def number_of_fr(self, RE_site):
