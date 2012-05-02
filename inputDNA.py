463c7c56e971507f9ed4af73838492f09e75e248
Heng-Chang Chen <chen.hc_evan@yahoo.de>
2012-05-02 19:06:26 +0200

    A class to open a file and a subclass to read a fasta file

--- inputDNA.py
+++ inputDNA.py
@@ -1,39 +1,45 @@
+# -*- coding:utf-8 -*-
+
 import re
 
-class Reader:
-  """A class checking a input sequence"""
+class Reader():
+
+   def __init__(self, file_name):
+      self.file = open(file_name)
+  
+   def read(self):
+      self.file.seek(0)
+      return self.file.read()
+    
+class Reader_fasta(Reader):
+   """A class checking a input sequence in a fasta file"""
 
    #Class variables
-   input_seq:""
-   correct_nucleotide: "ATCGN"
+   correct_nucleotide = "ATCGN"
    
    #Class methods
-   def __init__(self, input_seq):
-      """remove the first line in data without sequence"""
-      file = open("self.input_seq"))
-      for line in file:
-         if line[0] == ">":
-            pass
-         else:
-            print line
-      return
-  
-   def read_least(self, inout_seq):
-      """remove the least symbol in a sequence"""
-      file = open("self.input_seq")
-      read_seq = file.read()
-      read_seq = read_seq("\n", "")
-      return read_seq
+   
+   def read(self):
+      """clean the head"""
+      self.file.seek(0)
+      header = self.file.readline()
+      if header[0] == ">":
+         return self.file.read().replace("\n", "").replace("\r", "")
+      else:
+         raise Exception("It is not a fasta file!")
 
-      def seq_check(self, input_seq):
-         """check whether a sequence containing reasonable letters"""
-         self.input_seq = input_seq
-         file = open("self.input_seq")
-         read_seq = file.read()
-         for letter in read_seq:
-            if not letter in self.correct_nucleotide:
-               print "The given sequence contains wrong nucleotides."
-         return
+   def seq_check(self):
+      """check whether a sequence containing reasonable letters"""
+      read_seq = self.read()
+      for letter in read_seq:
+         if not letter in self.correct_nucleotide:
+            print "The given sequence contains wrong nucleotides."
+      return
+
+   def seq_letter_convert(self):
+      """convert letters to a correct form"""
+      read_seq = self.read()
+      return read_seq.upper()      
 
 
 if __name__ == '__main__':
