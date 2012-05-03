736ab3c352db114357d870b66662a20c821f3755
Olja <oliveravujatovic@yahoo.com>
2012-05-03 19:37:29 +0200

    It cuts the sequence in fragments and gives length of the fragments. It has a bag - it returns one fragment more.However, this fragment is empty

--- pCut.py
+++ pCut.py
@@ -16,51 +16,27 @@ class Plasmid:
     def cut(self, RE_site):
         fragments = self.sequence.split(RE_site)
         n = len(fragments)
-        fragments[0] = fragments[n-1] + fragments[0]
-        del(fragments[n-1])
-        x = [s.split(RE_site) for s in fragments[0]]
-        for x in range (n-1):
-            fragments[x] += RE_site
-        new_fragments = fragments[0].split(RE_site)
-        m = len(new_fragments)
-        if m > 1:
-            for i in range (m-1):
-                new_fragments[i] += RE_site
+        if n == 1:
+            return "There is no restriction enzyme site in your sequence"
         else:
             pass
+        fragments[0] = fragments[n-1] + fragments[0]
+        del(fragments[n-1])
+        for g in range (n-1):
+            fragments[g] += RE_site
+        temporary = fragments.pop(0)
+        new_fragments = temporary.split(RE_site)   
+        i = len(temporary.split(RE_site))
+        if i > 1:
+            for m in range (i-1):
+                new_fragments[m] += RE_site
+        else:
+            fragments[0] = temporary 
         fragments = new_fragments + fragments
         return fragments
-    
-    def cut_shorter(self, RE_site):
-        double_sequence = 2 * self.sequence
-        x = double_sequence.find(RE_site)
-        n = len(self.sequence)
-        if x>0:
-            fragments = double_sequence[x::]
-            fragments = fragments[:-(n-x)]
-            fragments = fragments.split(RE_site)
-            del(fragments[0])
-            k = len(fragments)
-            for y in range (k):
-                fragments[y] += RE_site
-        else:
-            return "There is no restriction ezyme cut in your sequence"
-        #new_fragments = fragments[0].split(RE_site)
-        #m = len(new_fragments)
-        #if m > 1:
-         #   for i in range (m-1):
-          #      new_fragments[i] += RE_site
-        #else:
-         #   pass
-        #fragments = new_fragments + fragments
-        return fragments
-    
-    def number_of_fr(self, RE_site):
-        return len(self.cut(RE_site))
         
     def fr_length(self, RE_site):
         x = self.cut(RE_site)
-        # Use list comprehension.
         return [len(string) for string in x]
     
 
