f93608a824343cf716898a28dfb2b3197b8ddd8c
gui11aume <guillaume.filion@gmail.com>
2012-05-04 17:28:15 +0200

    Wrote a function cut_ to replace cut.

--- pCut.py
+++ pCut.py
@@ -10,14 +10,26 @@ class Plasmid:
     #class variables
     
     #class methods
-    def __init__(self, sequence):
+    def __init__(self, sequence, check_and_fmt=True):
+        sequence = self.check_and_fmt(sequence)
         self.sequence = sequence
+
+    def check_and_fmt(self, sequence):
+        """Check that sequence contains only DNA letters, otherwise
+        raise an Exception, then format sequence to upper-case."""
+        sequence = sequence.upper()
+        if re.search('[^GATCN]', sequence):
+           raise Exception ('Not a DNA sequence')
+        else:
+           return sequence
         
     def cut(self, RE_site):
+        """Return a list of plasmid fragments delimited by the
+        specified RE_site, and 'None' if no site is found."""
         fragments = self.sequence.split(RE_site)
         n = len(fragments)
         if n == 1:
-            return "There is no restriction enzyme site in your sequence"
+            return None
         else:
             pass
         fragments[0] = fragments[n-1] + fragments[0]
@@ -40,7 +52,15 @@ class Plasmid:
         return [len(string) for string in x]
         #stg
     
-
-    
-
-    
+    def cut_(self, RE_site):
+        """Return a list of plasmid fragments delimited by the
+        specified RE_site, and 'None' if no site is found."""
+        try:
+           # Change plasmid offset (early exit if no RE site found).
+           start = (2*self.sequence).index(RE_site) + len(RE_site)
+           end = start + len(self.sequence) - len(RE_site)
+           # Use list comprehension to add site sequence to fragments.
+           return [ frag + RE_site for frag in \
+                      (2*self.sequence)[start:end].split(RE_site) ]
+        except ValueError:
+           return None
