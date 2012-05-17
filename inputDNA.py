197bcb1c8571befec17525e7a6a552cae7fb2388
Heng-Chang Chen <chen.hc_evan@yahoo.de>
2012-05-17 11:56:58 +0200

    funtions to read fasta, EMBL, GCG, GenBank, IG and plain files

--- inputDNA.py
+++ inputDNA.py
@@ -36,15 +36,17 @@ class DNAReader(Reader):
       self.file.seek(0)
       start = self.file.readline().upper()
       if start[0] == ">":
-         print "Input file belongs to the fasta file."
-      elif start[0:2] == "ID" and start[-1] == "//":
-         print "Input file belongs to the EMBL file."
+         print "This is a fasta file."
+      elif start[0:1] == "ID" and start[-1] == "//":
+         print "This is an EMBL file."
+      elif start[0:1] == "ID" and start[-1] != "//":
+         print "This is a GCG file.""
       elif start[0:5] == "LOCUS":
-         print "Input file belongs to the GenBank file."
+         print "This is a GenBank file."
       elif start[0] == ";":
-         print "Input file belongs to the IG file."
+         print "This is an IG file."
       elif re.search('[^GATCN]', start[0:-1]) == 0:
-         print "Input file belongs to the plain file."
+         print "This is a plain file."
       else:
          print "No corret file format is found."
       return
@@ -79,21 +81,72 @@ class FastaReader(DNAReader):
             print "The given sequence contains wrong nucleotides."
       return
 
+   def seq_read(self):
+      """read throgh only the nucleotide sequence in a fasta file"""
+      fc = self.read().upper()
+      fc_nucleotide = re.finall([GATCN]+,fc)
+      fc_seq = "".join.(fc_nucleotide[:])
+      return fc_seq
+
+class GCGReader(DNAReader):
+   """A class checking and orginasing an input sequence in a GCG file"""
+   
+   #Class methods
+
+   def seq_read(self);
+      fc = self.file.read().upper()
+      fc_start = fc[fc.index('..')+2:]
+      fc_nucleotide = re.findall([ATCG]+,fc_start)
+      fc_seq = "".join(fc_nucleotide[:])
+      return fc_seq
+
 class EMBLReader(DNAReader):
-   """A class checking an input sequence in a EMBL file"""
+   """A class checking and orginasing an input sequence in a EMBL file"""
+   
+   #Class methods
+   
+   def seq_start(self):
+      """find the first line of the aequence start"""
+      fc = self.file.readlines()
+      for fc_start in fc:
+         if fc_start[0:1] == "SQ":
+            fc_nucleotide += fc
+      
+   def seq_read(self):
+      """elimiating spaces within the sequence context"""
+      fc_nuc = self.seq_start().repalce("//","")
+      fc_seq = "".join(fc_nuc[:])
+      return fc_seq
+
+class GenBankReader(DNAReader):
+   """A class checking and orginasing an input sequence in a GenBank file"""
 
    #Class variables
    correct_nucleotide = "ATCGN"
 
    #Class methods
       
-   def read_seq(self):≈
+   def seq_read(self):≈
       fc = self.file.read()
       # Substring from after "ORIGIN" till end of file.
       fc_start = fc[fc.index('ORIGIN')+6:]
       fc_nucleotide = re.findall([GATCN]+,fc_start)
       fc_seq = "".join(fc_nucleotide[:])
       return fc_seq
+
+class IGReader(DNAReader):
+
+  #Class methods
+  
+  def seq_read(self):
+     fc = self.file.read().replace(";","").repace("comment","").upper()
+     fc_seq = re.findall([GATCN]+,fc)
+     return fc_seq
+
+class PlainReader(DNAReader):
+
+   def seq_read(self):
+      return self.file.read()
 
 
 if __name__ == '__main__':
